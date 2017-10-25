1d862a67309303d32f08c42c921cdcdace365f12
Phillip <pc494@cam.ac.uk>
2017-10-25 10:38:11 +0100

    deletions and moves

--- conversion_ase_pymatgen.py
+++
@@ -1,8 +0,0 @@
-import pymatgen as pmg
-from pymatgen.io import ase
-
-si = pmg.Element("Si")
-lattice = pmg.Lattice.cubic(5.431)
-silicon = pmg.Structure.from_spacegroup("Fd-3m",lattice, [si], [[0, 0, 0]])
-
-silicon_ase = ase.AseAtomsAdaptor.get_atoms(silicon)
